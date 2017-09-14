85df570b58541922866c8301fe129dd19f77f2d3
Melinda Fernandes <melindafernandes1993@gmail.com>
2017-09-14 18:27:15 +0100

    json file mismatch

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'httparty'
 require_relative '../weathers'
 require_relative '../generator/country_generator'
-require_relative '../weather_conditions/weather_conditions_parser'
+require_relative '../weather_conditions/weather_condition_parser'
 
 RSpec.configure do |config|
   config.color = true
--- spec/weather_codes_spec.rb
+++ spec/weather_codes_spec.rb
@@ -4,11 +4,11 @@ describe ConditionsCode do
 
 	before(:all) do
 		@first_condition = WeatherConditions::ConditionsCode.new("301")
-		@second_condition = WeatherConditions::ConditionsCode.new("301")
+		@second_condition = WeatherConditions::ConditionsCode.new("501")
 	end
 
 	it 'should return a string for the description associated with the id' do
-		puts @first_condition.meaning
+		expect(@first_condition.meaning).to be_a String
 	end
 
 end
