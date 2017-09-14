0138044603fbfd0719717a1d8f97a41161833402
Tech-A03 <tech-a03@Tech-A03s-MacBook-Air-2.local>
2017-09-14 16:46:42 +0100

    tests implemented

--- spec/current_weather_spec.rb
+++ spec/current_weather_spec.rb
@@ -1,34 +1,35 @@
 include Weathers
-include Generator
 
 describe CurrentWeather do
 
 	before(:all) do
 		#Generate random captial city
-		@capital_city = Generator::RandomCountryGenerator.new
-		puts @captial_city.generate_country
-		# @captial_city.generate_country
+		@generator = Generator::RandomCountryGenerator.new
+        @generator.generate_country
 		#Obtaining weather json object
-		# @current_weather = Weathers::CurrentWeather.new(@captial_city.capital_name,@captial_city.country_code).get_single_body
+		@current_weather = Weathers::CurrentWeather.new(@generator.capital_name,@generator.country_code).get_single_body
 	end
 
 	it 'should return matching country code and city name' do
-
+		expect(@current_weather['name'].delete(' ')).to eq @generator.capital_name
+		expect(@current_weather['sys']['country']).to eq @generator.country_code
 	end
 
-	it 'should return a weather JSON object with at most 12 main keys' do
-
+	it 'should return a current weather JSON object with at most 12 main keys' do
+		expect(@current_weather.keys.length).to be_between(11,12)
 	end
 
-	it 'should return coordinates that are a JSON object, with longitude an latitude floats' do
-
+	it 'should return coordinates that are a Hash, with longitude an latitude floats' do
+		expect(@current_weather['coord']).to be_a(Hash)
+		expect(@current_weather['coord']['lon']).to be_a(Float)
+		expect(@current_weather['coord']['lat']).to be_a(Float)
 	end
 
 	it 'should return a weather array' do
-
+		expect(@current_weather['weather']).to be_a(Array)
 	end
 
-	it 'should return a JSON object inside the weather array that' do
+	it 'should return Hashes inside the weather array that has description that matches to their id number' do
 
 	end
 
